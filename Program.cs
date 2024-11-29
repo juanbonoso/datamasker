19b419bc6c1ab0f35653e4da749dfff26d5c8bac
juanbonoso <juanbonoso@gmail.com>
2024-11-29 12:08:59 -0500

    base project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataMaskerExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
